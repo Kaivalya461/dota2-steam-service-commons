80d0256c9dbeed5350aaa3574d7fd7a767dec3e2
Kaivalya Thanekar <kaivalyathanekar@gmail.com>
2024-03-02 23:46:34 +0530

    Add function to filter matches based on fromDate and toDate

--- src/main/java/com/kv/service/CommonUtilityService.java
+++ src/main/java/com/kv/service/CommonUtilityService.java
@@ -2,11 +2,21 @@ package com.kv.service;
 
 import com.kv.dao.MatchDetailsDaoFileStorageImpl;
 import com.kv.matchdetails.dto.MatchDetailsDto;
+import com.kv.matchdetails.dto.MatchesDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Collection;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 @Service
 public class CommonUtilityService {
+
+    private static final short FIVE_AND_HALF_HOURS_TIME_IN_SECONDS = 19800;
     @Autowired
     private MatchDetailsDaoFileStorageImpl matchDetailsDao;
 
@@ -22,5 +32,13 @@ public class CommonUtilityService {
             return matchDetailsDao.save(obj);
         else
             return null;
+    }
+
+    public static Set<MatchesDto> getMatchesPlayedInBetween(LocalDate fromDate, LocalDate toDate, Collection<MatchesDto> matchesDtoCollection) {
+        return matchesDtoCollection
+                .stream()
+                .filter(match -> (fromDate.isBefore(LocalDateTime.ofEpochSecond(match.getStart_time(), 0, ZoneOffset.ofTotalSeconds(FIVE_AND_HALF_HOURS_TIME_IN_SECONDS)).toLocalDate()) || fromDate.isEqual(LocalDateTime.ofEpochSecond(match.getStart_time(), 0, ZoneOffset.ofTotalSeconds(FIVE_AND_HALF_HOURS_TIME_IN_SECONDS)).toLocalDate())
+                        && (toDate.isAfter(LocalDateTime.ofEpochSecond(match.getStart_time(), 0, ZoneOffset.ofTotalSeconds(FIVE_AND_HALF_HOURS_TIME_IN_SECONDS)).toLocalDate()) || toDate.isEqual(LocalDateTime.ofEpochSecond(match.getStart_time(), 0, ZoneOffset.ofTotalSeconds(FIVE_AND_HALF_HOURS_TIME_IN_SECONDS)).toLocalDate()))))
+                .collect(Collectors.toSet());
     }
 }
